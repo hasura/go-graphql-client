7a79ab17a1d7ca7e0a521abed52809f1969c353a
Rafał Kałuski <rafal.kaluski@asseco.pl>
2024-06-28 12:01:54 +0200

    feat: add ExecRawWithExtensions method for retrieving extensions in response

--- graphql_test.go
+++ graphql_test.go
@@ -470,6 +470,81 @@ func TestClient_Exec_QueryRaw(t *testing.T) {
 	}
 }
 
+// Test exec pre-built query, return raw json string and map
+// with extensions
+func TestClient_Exec_QueryRawWithExtensions(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/graphql", func(w http.ResponseWriter, req *http.Request) {
+		body := mustRead(req.Body)
+		if got, want := body, `{"query":"{user{id,name}}"}`+"\n"; got != want {
+			t.Errorf("got body: %v, want %v", got, want)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		mustWrite(w, `{"data": {"user": {"name": "Gopher"}}, "extensions": {"domain": "users", "database": {"id": 1, "name": "users_db"}}"}`)
+	})
+	client := graphql.NewClient("/graphql", &http.Client{Transport: localRoundTripper{handler: mux}})
+
+	var database struct {
+		ID   int    `graphql:"id"`
+		Name string `graphql:"name"`
+	}
+
+	_, extensions, err := client.ExecRawWithExtensions(context.Background(), "{user{id,name}}", map[string]interface{}{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := len(extensions), 2; got != want {
+		t.Errorf("got len(extensions): %q, want: %q", got, want)
+	}
+
+	var domain string
+	err = json.Unmarshal(extensions["domain"], &domain)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := domain, "users"; got != want {
+		t.Errorf("got domain: %q, want: %q", got, want)
+	}
+
+	err = json.Unmarshal(extensions["database"], &database)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := database.ID, 1; got != want {
+		t.Errorf("got database.ID: %q, want: %q", got, want)
+	}
+	if got, want := database.Name, "users_db"; got != want {
+		t.Errorf("got database.Name: %q, want: %q", got, want)
+	}
+}
+
+// Test exec pre-built query, return raw json string and map
+// with extension set to `null`
+func TestClient_Exec_QueryRawWithNullExtension(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/graphql", func(w http.ResponseWriter, req *http.Request) {
+		body := mustRead(req.Body)
+		if got, want := body, `{"query":"{user{id,name}}"}`+"\n"; got != want {
+			t.Errorf("got body: %v, want %v", got, want)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		mustWrite(w, `{"data": {"user": {"name": "Gopher"}}, "extensions": {"domain": null}}`)
+	})
+	client := graphql.NewClient("/graphql", &http.Client{Transport: localRoundTripper{handler: mux}})
+
+	_, extensions, err := client.ExecRawWithExtensions(context.Background(), "{user{id,name}}", map[string]interface{}{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got := extensions["domain"]; got != nil {
+		t.Errorf("got non-nil extensions[\"domain\"]: %q, want: nil", got)
+	}
+}
+
 // localRoundTripper is an http.RoundTripper that executes HTTP transactions
 // by using handler directly, instead of going over an HTTP connection.
 type localRoundTripper struct {
